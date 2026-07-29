e1de44e3b51e3d1e9eceb36030f34c8d1c8495d4
Katargina <katrin-kate@ukr.net>
2026-07-29 02:35:22 +0200

    fixed

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -1,21 +1,21 @@
 const openBtnEl = document.querySelector('[data-action="open"]');
 const burgerMenuEl = document.querySelector('[data-visible]');
 const closeActionEls = document.querySelectorAll('[data-action="close"]');
-const mobileMenuLinkEls = document.querySelectorAll('.mobile-link');
+const mobileMenuLinkEls = document.querySelectorAll('[data-menu-link]');
 
 if (openBtnEl && burgerMenuEl) {
   const openMenu = () => {
     burgerMenuEl.dataset.visible = 'open';
     burgerMenuEl.setAttribute('aria-hidden', 'false');
     openBtnEl.setAttribute('aria-expanded', 'true');
-    document.body.classList.add('menu-open');
+    document.body.dataset.lock = 'true';
   };
 
   const closeMenu = () => {
     burgerMenuEl.dataset.visible = 'close';
     burgerMenuEl.setAttribute('aria-hidden', 'true');
     openBtnEl.setAttribute('aria-expanded', 'false');
-    document.body.classList.remove('menu-open');
+    document.body.dataset.lock = 'false';
   };
 
   openBtnEl.addEventListener('click', openMenu);
